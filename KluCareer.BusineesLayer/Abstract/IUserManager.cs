f014c5461255a06e0694709f81956f56920bf030
halitakkus <halitakkus03@gmail.com>
2020-12-19 21:30:54 +0300

    İş katmanı hazırlandı..

---
+++ KluCareer.BusineesLayer/Abstract/IUserManager.cs
@@ -0,0 +1,16 @@
+﻿using KluCareer.BusineesLayer.Result;
+using KluCareer.Entities.Concrate.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KluCareer.BusineesLayer.Abstract
+{
+    public interface IUserManager
+    {
+        IResult Add(User user);
+        IResult Update(User user);
+        IResult Remove(User user);
+        IDataResult<List<User>> GetList();
+    }
+}
---
+++ KluCareer.BusineesLayer/Concrate/UserManager.cs
@@ -0,0 +1,73 @@
+﻿using KluCareer.BusineesLayer.Abstract;
+using KluCareer.BusineesLayer.Result;
+using KluCareer.DataAccessLayer.Abstract;
+using KluCareer.DataAccessLayer.Concrate.EntityFramework;
+using KluCareer.Entities.Concrate.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KluCareer.BusineesLayer.Concrate
+{
+    public class UserManager : IUserManager
+    {
+        private IUserDal _userDal = new EfUserDal();
+       
+        public IResult Add(User user)
+        {
+            bool isAdded = _userDal.Add(user);
+
+            if (!isAdded)
+            {
+                var errorResult = new ErrorResult();
+                errorResult.AddMessage("ObjectNotAdded","Veri bir hata sonucu eklenemedi.");
+                return errorResult;
+            }
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<User>> GetList()
+        {
+            var users = _userDal.GetList();
+            return new SuccessDataResult<List<User>>(users);
+        }
+
+        public IResult Remove(User user)
+        {
+            var remove = _userDal.Remove(user);
+            if (!remove)
+            {
+                var error = new ErrorResult();
+
+                error.AddMessage("notDeleted", "veri silinemedi");
+                return error;
+            }
+            return new SuccessResult();
+        }
+
+        public IResult Update(User user)
+        {
+           
+            var error = new ErrorResult();
+            var isValid = _userDal.GetById(user.Id);
+            if (isValid==null)
+            {
+                error.AddMessage("objectNotFount", "Veri bulunamadı.");
+                return error;
+            }
+
+            if (user==null)
+            {
+                error.AddMessage("objectNotFount","Doğrulama başarısız..");
+                return error;
+            }
+            var isUpdate = _userDal.Update(user);
+            if (!isUpdate)
+            {
+                error.AddMessage("failed", "Veri güncellenemedi.");
+                return error;
+            }
+            return new SuccessResult();
+        }
+    }
+}
