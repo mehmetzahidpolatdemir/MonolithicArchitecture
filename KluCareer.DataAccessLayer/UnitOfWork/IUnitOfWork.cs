75866e79bd269e07bdb5fc44cd7486e88eb9e585
halitakkus <halitakkus03@gmail.com>
2020-12-19 21:32:00 +0300

    Unit Of Work deseni yapıldı.

---
+++ KluCareer.DataAccessLayer/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KluCareer.DataAccessLayer.UnitOfWork
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        bool Commit(bool state = true);
+    }
+}
